9bc191cb5b120cffc66ae1f4310c1ad840294a8b
Omar Monrroy <ogcm.kael@hotmail.com>
2019-10-19 12:57:45 -0500

    Configuración básica del servidor.
    Esta configuración sirve para que el servidor arranque.

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/', (req, res) => res.status(500).json(req.body));
+
+app.listen(process.env.PORT, (err) => {
+    if (err) {
+        console.error(err);
+    }
+    console.log('Servidor corriendo en puerto', process.env.PORT);
+});
