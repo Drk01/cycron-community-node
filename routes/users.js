af3198141f902b1783dd8467e1dc1c6211d6f2e2
Omar Monrroy <ogcm.kael@hotmail.com>
2019-10-20 01:03:04 -0500

    Creando método signup.
    Este método se encargará de recibir los datos del usuario y almacenarlos en la base de datos.

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const Controller = require('../controllers/UsersController');
 
+router.post('/signup', async (req, res) => await Controller.signUp(req, res));
 
 module.exports = router;
