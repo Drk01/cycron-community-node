9b0c36880e44c13cad3372b9763bc07bccae6d4a
Omar Monrroy <ogcm.kael@hotmail.com>
2019-10-20 01:39:49 -0500

    Método signUp en UsersController

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,5 +1,25 @@
-const signUp = async (req,res) =>{
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
+const signUp = async (req,res) =>{
+    try {
+        await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: bcrypt.hashSync(req.body.password, 10)
+        });
+
+        return res.json({
+            ok: true,
+            message: 'Usuario registrado satisfactoriamente'
+        });
+
+    } catch (message) {
+        return res.status(500).json({
+            ok: false,
+            message
+        });
+    }
 };
 
 
