c54227f01341adc15e7a697c082a77b51d6c72d0
Omar Monrroy <ogcm.kael@hotmail.com>
2019-10-21 03:29:19 -0500

    Middleware auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,9 +1,20 @@
 const JWT = require('jsonwebtoken');
+const User = require('../models/User');
 
-const auth = (req, res, next) => {
+const auth = async (req, res, next) => {
     const token = req.get('token');
 
     if (JWT.verify(token, process.env.JWTKEY)) {
+
+        const decoded = JWT.decode(token);
+        const usuario = await User.findOne({
+            where: {
+                email: decoded.data
+            }
+        })
+
+        req.user = usuario.id;
+
         next();
     } else {
         return res.status(401).json({
