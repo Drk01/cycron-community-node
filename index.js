bb5fe1cac1500c2bb2395af45f48f1a4a0f1a0d9
Omar Monrroy <ogcm.kael@hotmail.com>
2019-10-21 01:42:39 -0500

    Implementando express-fileupload

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ require('./services/database');
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
+const fileUpload = require('express-fileupload');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(fileUpload());
 app.use(require('./routes/index'));
 
 app.listen(process.env.PORT, (err) => {
