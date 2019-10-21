d0b0b27f82893177e65631f80888a689515e5d9a
Omar Monrroy <ogcm.kael@hotmail.com>
2019-10-20 23:19:01 -0500

    Ruta /posts/all

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,9 +1,29 @@
+const Post = require('../models/Post');
 
+const all = async (req, res) => {
+    
+    try {
+        //TODO: Hacer que los registros salgan ordenados de forma descendente 
+        const posts = await Post.findAll({});
+
+        return res.json({
+            ok: true, 
+            posts
+        });
+
+    } catch (message) {
+        return res.status(500).json({
+            ok: false,
+            message
+        })
+    }
+
+};
 
 
 
 
 
 module.exports = {
-    
+    all
 }
--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
+const Controller = require('../controllers/PostController');
+const auth = require('../middlewares/auth');
 
-
-
-
+router.get('/posts/all', async (req, res) => await Controller.all(req, res));
 
 
 
