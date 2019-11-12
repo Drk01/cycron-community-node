c0667301acc32add8d5c7fe46d0d213782338483
Omar Monrroy <ogmonrroy@gmail.com>
2019-11-12 02:40:11 -0600

    Code optimization

--- routes/posts.js
+++ routes/posts.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const Controller = require('../controllers/PostController');
 const auth = require('../middlewares/auth');
 
-router.get('/posts/all', auth, async (req, res) => await Controller.all(req, res));
+router.get('/posts/all', async (req, res) => await Controller.all(req, res));
 router.get('/posts', async (req, res) => await Controller.posts(req, res));
 
 router.post('/posts/store', async (req, res) => await Controller.store(req, res));
