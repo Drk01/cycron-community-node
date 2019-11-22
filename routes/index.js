634a475208ad103c752b8b3549b9cad5ba628877
Omar Monrroy <ogmonrroy@gmail.com>
2019-11-22 14:19:27 -0600

    Creación y declaración del archivo de rutas de la sección de The Brain

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 
 router.use('/v1/auth', require('./users'));
 router.use('/v1/', require('./posts'));
+router.use('/v1', require('./the-brain'));
 
 module.exports = router;
