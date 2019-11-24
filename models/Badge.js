f92c49892ad81bb999754a9277412ca204c92fc1
Omar Monrroy <ogmonrroy@gmail.com>
2019-11-24 01:26:15 -0600

    Modelo Badge refactorizado

--- models/Badge.js
+++ models/Badge.js
@@ -2,20 +2,34 @@ const db = require('../services/database');
 const Sequelize = require('sequelize');
 
 const Badge = db.define('Badge', {
-    crypto: {
+    //Empresa(s) anunciante(s)
+    companies: {
         type: Sequelize.STRING,
         allowNull: false
     },
+    //Porcentaje de votos del evento
+    votes: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    },
+    //Título del evento
     title: {
         type: Sequelize.STRING,
         allowNull: false
     },
+    //Fecha del evento
     date: {
-        type: Sequelize.STRING,
+        type: Sequelize.DATE,
         allowNull: false
     },
+    // Descripción del evento
     description:{
         type: Sequelize.STRING
+    },
+    // Fuente del evento
+    source:{
+      type: Sequelize.STRING,
+      allowNull: false
     }
 });
 
